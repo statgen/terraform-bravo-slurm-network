a543067cc274831b6eaf4dacab948012348dc904
Colin Gross <grosscol@gmail.com>
2022-04-05 13:33:37 -0400

    Add shared app/ storage for cluster.

--- main.tf
+++ main.tf
@@ -4,18 +4,34 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
-resource "google_filestore_instance" "shared_storage" {
-  name = "slrum-shared-storage"
+resource "google_filestore_instance" "shared_home" {
+  name = "slurm-shared-home"
   zone = "us-central1-c"
   tier = "BASIC_SSD"
 
   file_shares {
     capacity_gb = 2560
-    name        = "slurm_share"
+    name        = "slurm_home"
   }
 
   networks {
-    network = "bravo-slurm-vpc"
+    network = google_compute_network.cluster_network.name
+    modes   = ["MODE_IPV4"]
+  }
+}
+
+resource "google_filestore_instance" "shared_app" {
+  name = "slurm-shared-app"
+  zone = "us-central1-c"
+  tier = "BASIC_SSD"
+
+  file_shares {
+    capacity_gb = 2560
+    name        = "slurm_app"
+  }
+
+  networks {
+    network = google_compute_network.cluster_network.name
     modes   = ["MODE_IPV4"]
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,17 @@
-output "filestore_ip" {
-  value = google_filestore_instance.shared_storage.networks.0.ip_addresses.0
+output "filestore_ip_home" {
+  value = google_filestore_instance.shared_home.networks.0.ip_addresses.0
 }
 
-output "filestore_share" {
-  value = google_filestore_instance.shared_storage.file_shares[0].name
+output "filestore_name_home" {
+  value = google_filestore_instance.shared_home.file_shares[0].name
+}
+
+output "filestore_ip_app" {
+  value = google_filestore_instance.shared_app.networks.0.ip_addresses.0
+}
+
+output "filestore_name_app" {
+  value = google_filestore_instance.shared_app.file_shares[0].name
 }
 
 output "network_name" {
